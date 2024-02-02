0bbc267d49cdff5fd3b376d327853e5152f74d3e
gfredes- <gfredes-@student.42malaga.com>
2024-02-02 23:40:32 +0100

    lexer_pipes.c cleaned

--- lexer_pipes.c
+++ lexer_pipes.c
@@ -76,34 +76,6 @@ t_list	*split_pipe(t_list *list, int i)
 	return (list);
 }
 
-/*t_list	**handle_quotes(t_list **list)
-{
-	char	*tmp_word;
-
-	tmp_word = (*list)->content;
-	if (tmp_word[0] == '\'' || tmp_word[0] == '\"')
-	{
-		*list = (*list)->next;
-	}
-	return (list);
-}
-
-t_list **handle_pipes(t_list **list, int i)
-{
-	char	*tmp_word;
-	int		len;
-	int		n_pipes;
-
-	tmp_word = (*list)->content;
-	len = ft_strlen(tmp_word);
-	n_pipes = get_pipe_nbr(tmp_word, i);
-	if (len > 0 && n_pipes > 0)
-	{
-		*list = split_pipe(*list, i);
-	}
-	return (list);
-}*/
-
 t_list	**split_pipes(t_list **list)
 {
 	t_list	**tmp;
